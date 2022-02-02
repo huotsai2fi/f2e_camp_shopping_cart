6749f53edb1d6a60549494a6fc57b36890fd6ecf
April Hung <huotsai2fi@gmail.com>
2022-02-03 00:43:32 +0800

    feat: add form verification on registration page

--- js/registration.js
+++ js/registration.js
@@ -1,32 +1,72 @@
-$("#new-password").keyup(function checkLength() {
-  if ($(this).val().length < 8) {
-    $(this).siblings("small").text("密碼長度不足").css("color", "red");
-  } else {
-    $(this).siblings("small").text("密碼長度OK").css("color", "green");
-  }
-});
+// $("#new-password").keyup(function checkLength() {
+//   if ($(this).val().length < 8) {
+//     $(this).siblings("small").text("密碼長度不足").css("color", "red");
+//   } else {
+//     $(this).siblings("small").text("密碼長度OK").css("color", "green");
+//   }
+// });
 
-$("#verify-password").keyup(function checkIdentity() {
-  if ($(this).val() != $("#new-password").val()) {
-    $(this).siblings("small").text("密碼不相符").css("color", "red");
-  } else {
-    $(this).siblings("small").text("密碼相符").css("color", "green");
-  }
-});
+// $("#verify-password").keyup(function checkIdentity() {
+//   if ($(this).val() != $("#new-password").val()) {
+//     $(this).siblings("small").text("密碼不相符").css("color", "red");
+//   } else {
+//     $(this).siblings("small").text("密碼相符").css("color", "green");
+//   }
+// });
 
 $("#register-form").validate({
-  // initialize plugin
-  // your rules & options,
+  rules: {
+    username: {
+      required: true,
+    },
+    email: {
+      required: true,
+      email: true,
+    },
+    "new-account": {
+      required: true,
+    },
+    "new-password": {
+      required: true,
+      minlength: 8,
+    },
+    "verify-password": {
+      required: true,
+      equalTo: "#new-password",
+    },
+  },
+
+  messages: {
+    username: {
+      required: "必填",
+    },
+    email: {
+      required: "必填",
+      email: "格式不符",
+    },
+    "new-account": {
+      required: "必填",
+    },
+    "new-password": {
+      required: "必填",
+      minlength: "長度至少為8",
+    },
+    "verify-password": {
+      required: "必填",
+      equalTo: "密碼不一致",
+    },
+  },
   submitHandler: function (form) {
     let username = $("#username").val();
     let email = $("#email").val();
+    let newAccount = $("#new-account").val();
     let newPasswd = $("#new-password").val();
     let verifyPasswd = $("#verify-password").val();
 
     $.post(
       "http://140.127.196.92:1234/login",
       {
-        account: email,
+        account: newAccount,
         password: newPasswd,
       },
       function (data, status) {
@@ -34,6 +74,7 @@ $("#register-form").validate({
           $.cookie("authKey", data);
           $.cookie("username", username);
           $.cookie("email", email);
+          $.cookie("account", newAccount);
         }
         window.location.replace("shopping_cart.html");
       }
--- js/shopping_cart.js
+++ js/shopping_cart.js
@@ -1,13 +1,14 @@
 $(document).ready(function () {
   $("#username").val($.cookie("username"));
   $("#email").val($.cookie("email"));
+  $("#account").val($.cookie("account"));
 });
 
 $("#shopping-cart-form").validate({
   // initialize plugin
   // your rules & options,
   submitHandler: function (form) {
-    window.location.replace("confirm.html");
+    // window.location.replace("confirm.html");
     return false;
   },
 });
